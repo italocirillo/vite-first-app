d20c402af16679f8406e71704d33d5fa83859cd3
italocirillo <italocirillo@gmail.com>
2023-04-12 14:58:08 +0200

    ESEMPIO COMPLETO

---
+++ src/components/AppBanner.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    name: "AppBanner",
+    data() {
+        return {
+            text: "lorem",
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<template>
+    <h2>Banner</h2>
+    <p>{{ text }}</p>
+</template>
+
+<style>
+h2 {
+    color: aquamarine;
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: "AppTitle",
+    data() {
+        return {
+            title: "titolo"
+        }
+    }
+}
+</script>
+
+<template>
+    <h2>{{ title }}</h2>
+</template>
+
+<style>
+h2 {
+    color: royalblue;
+}
+</style>
